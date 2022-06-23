e7f0bab016f6d5855b5d81ac22f1b57d614f7f17
mobinmp <107753738+mobinmp@users.noreply.github.com>
2022-06-23 19:51:04 +0600

    project
    ggg

---
+++ psd&html/resources/js/main.js
@@ -0,0 +1,11 @@
+// MIX IT UP
+var mixer = mixitup('.protfulio-grid'); 
+
+//scroll
+function openToggle() {
+    document.getElementById('navbare').style.width = '100%';
+}
+ function closeMenu() {
+    document.getElementById('navbare').style.width = '0%';
+ }
+ new WOW().init();
